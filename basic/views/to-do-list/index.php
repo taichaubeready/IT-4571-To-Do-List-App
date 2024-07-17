652f12a80e970e78d56eb2f7cb1e53a0eaa1d017
taichaubeready <tai.chau@beready.academy>
2024-07-17 18:16:23 +0700

    IT-4571 In Progress

--- basic/views/to-do-list/index.php
+++ basic/views/to-do-list/index.php
@@ -30,36 +30,34 @@ AppAsset::register($this);
 <body>
     <?php $this->beginBody() ?>
 
-    <div class="" x-data="{ todos: [], userInput: '' }">
-        
+    <div class="" x-data="{ todos: [], userInput: '', message: 'more to go!' }">
+
+        <!-- Counts Todos -->
+        <div class="">
+            <h1>
+                To Dos: <span x-text="todos.length"></span> |
+                Completed: <span x-text="todos.filter((todo) => todo.completed).length"></span>
+            </h1>
+        </div>
 
         <br>
 
-        <!-- Form -->
-        <form x-on:submit.prevent>
-            <input x-model="userInput" type="text" placeholder="Enter todos item">
-            <button type="button"
-                @click="if(userInput.trim().length > 0) { todos.push({ name: userInput, completed: false }) }; userInput = '' ">Add</button>
-        </form>
+        <!-- Counts text Todos -->
+        <div>
+            <h1>
+                <span x-text="todos.length"></span> <span x-text="message"></span>
+            </h1>
+        </div>
+
+        <br>
 
         <!-- Foreach Loop Todos -->
-        <!-- <template x-for="(todo, index) in todos">
-            <div>
+        <template x-for="(todo, index) in todos">
+            <div class="bg-red-500 text-white p-2 mb-4 rounded-md w-1/2">
                 <input type="checkbox" x-model="todo.completed">
                 <span x-text="todo.name" :class="{ 'completed': todo.completed }"></span>
-                <button type="button"
-                    @click="todos = todos.filter((currTodo, todoIndex) => { return index !== todoIndex })"
-                    class="delete">x</button>
-            </div>
-        </template> -->
-
-        <template x-for="todo in todos">
-            <div>
-                <input type="checkbox" x-model="todo.completed">
-                <span x-text="todo.name" :class="{ 'completed': todo.completed }"></span>
-                <button type="button"
-                    @click="todos = todos.filter((currTodo, todoIndex) => { return index !== todoIndex })"
-                    class="delete">x</button>
+                <button type="button" @click="todos = todos.filter((currTodo, todoIndex) => todoIndex !== index)"
+                    class="delete text-white">Remove</button>
             </div>
         </template>
 
@@ -70,13 +68,12 @@ AppAsset::register($this);
 
         <br><br>
 
-        <!-- Counts Todos -->
-        <div class="">
-            <h1>
-                <span x-text="todos.filter((todo) => todo.completed).length"></span> /
-                <span x-text="todos.length"></span> Todos Completed
-            </h1>
-        </div>
+        <!-- Form -->
+        <form x-on:submit.prevent>
+            <input x-model="userInput" type="text" placeholder="Enter todos item">
+            <button type="button"
+                @click="if(userInput.trim().length > 0) { todos.push({ name: userInput, completed: false }) }; userInput = '' ">Add</button>
+        </form>
 
     </div>
 
